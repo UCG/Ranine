5cc0f0864b3df61fe37fde2cec0299bdec5fcf77
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2021-06-29 16:42:10 -0400

    Quick fix.

--- src/Helper/Drupal/DatabaseHelpers.php
+++ src/Helper/Drupal/DatabaseHelpers.php
@@ -2,6 +2,8 @@
 
 declare (strict_types = 1);
 
+namespace Ranine\Helper\Drupal;
+
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Database\DatabaseExceptionWrapper;
 use Ranine\Helper\ThrowHelpers;
