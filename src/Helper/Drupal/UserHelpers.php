920128f130a5a325ecfbd73bad74be9d54d503c2
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2021-07-13 17:32:07 -0400

    Created attribute hashing helper method.

---
+++ src/Helper/Drupal/UserHelpers.php
@@ -0,0 +1,179 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Ranine\Helper\Drupal;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Ranine\Helper\StringHelpers;
+use Ranine\Helper\ThrowHelpers;
+use Ranine\Iteration\ExtendableIterable;
+
+/**
+ * Contains Drupal-related helper methods dealing with Drupal users.
+ */
+final class UserHelpers {
+
+  /**
+   * Empty private constructor to ensure no one instantiates this class.
+   */
+  private function __construct() {
+  }
+
+  /**
+   * Gets the SHA-1 hash of a given set of attributes for a given set of users.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Entity type manager.
+   * @param iterable<int> $sortedUids
+   *   UIDs in sorted (low to high) order with no duplicates.
+   * @param string[] $sortedAttributeTypes
+   *   Attribute types in sorted (low to high) binary order with no duplicates.
+   * @param callable $binaryAttributeRepresentationRetrieval
+   *   Gets the binary attribute representation for a given user. Is of the form
+   *   (\Drupal\user\UserInterface $user, string $attributeType) : string.
+   * @param callable $userFilter
+   *   Filters users out of the hash. Of the form
+   *   (\Drupal\user\UserInterface) : bool, this method returns TRUE if a user
+   *   should be included in the normal hash computation, or FALSE if the user's
+   *   binary representation should just be the empty string.
+   * @param int $userLoadPageSize
+   *   The maximum number of users to try to load at once. Can be used to ease
+   *   memory burdens.
+   *
+   * @return string
+   *   SHA-1 hash of a string of the following form:
+   *   {serialization of first attribute type of first user}:
+   *   {serialization of second attribute type of first user}:...
+   *   |{serialization of first attribute type of second user}:...
+   *   Here ":" represents the ASCII record separator and "|" represents the
+   *   ASCII group separator. These separators are escaped from the
+   *   serializations with the ASCII ESC character (which is also used to escape
+   *   the ESC character itself). The attribute type serializations are ordered
+   *   in an ascending sorted fashion (higher binary strings later on), with
+   *   duplicate attribute types removed. The users are also ordered in an
+   *   ascending fashion in the string above, with duplicate users removed.
+   *   If a user does not exist or it doesn't pass through the fitler, its
+   *   serialization string is the empty string.
+   *
+   * @throws \InvalidArgumentException
+   *   Thrown if $sortedAttributeTypes contains a value that is not a string.
+   * @throws \InvalidArgumentException
+   *   Thrown if $sortedAttributeTypes is not in sorted order without
+   *   duplicates.
+   * @throws \InvalidArgumentException
+   *   Thrown if $sortedUids is not in sorted order without duplicates.
+   * @throws \InvalidArgumentException
+   *   Thrown if $sortedUids contains a value that is not a non-negative
+   *   integer.
+   * @throws \InvalidArgumentException
+   *   Thrown if $userLoadPageSize is less than or equal to zero.
+   * @throws \LogicException
+   *   Thrown if $binaryAttributeRepresentationRetrieval does not return a
+   *   string.
+   */
+  public static function getAttributeHash(EntityTypeManagerInterface $entityTypeManager,
+    iterable $sortedUids,
+    array $sortedAttributeTypes,
+    callable $binaryAttributeRepresentationRetrieval,
+    callable $userFilter,
+    int $userLoadPageSize = 10000) : string {
+    ThrowHelpers::throwIfLessThanOrEqualToZero($userLoadPageSize, 'userLoadPageSize');
+    $lastAttributeType = NULL;
+    foreach ($sortedAttributeTypes as $attributeType) {
+      if (!is_string($attributeType)) {
+        throw new \InvalidArgumentException('$sortedAttributeTypes contains a value that is not a string.');
+      }
+      if ($lastAttributeType !== NULL && strcmp($lastAttributeType, $attributeType) >= 0) {
+        throw new \InvalidArgumentException('$sortedAttributeTypes is not in sorted ascending order without duplicates.');
+      }
+    }
+
+    // Collect "pages" of UIDs to be loaded at once.
+    /** @var iterable<int[]>|\Ranine\Iteration\ExtendableIterable */
+    $pages = ExtendableIterable::from((function () use ($sortedUids, $userLoadPageSize) {
+      $pageId = 0;
+      $page = [];
+      foreach ($sortedUids as $uid) {
+        if (count($page) >= $userLoadPageSize) {
+          yield $pageId => $page;
+          $page = [];
+          $pageId++;
+        }
+        $page[] = $uid;
+      }
+      yield $pageId => $page;
+    })());
+
+    $userStorage = $entityTypeManager->getStorage('user');
+    $entityType = $userStorage->getEntityType();
+    $wasStaticCacheEnabled = $entityType->isStaticallyCacheable();
+    try {
+      // Disable static caching (for performance reasons) while we loop through
+      // the users -- otherwise all the users we load would end up in memory and
+      // not be garbage collected.
+      $entityType->set('static_cache', FALSE);
+
+      // Create the hash context, used to compute a hash of a long string
+      // without first loading the entire string into memory.
+      /** @var \HashContext|resource */
+      $hashContext = hash_init('sha1');
+      // We use the record and group separator to divide attributes and users,
+      // respectively, so we want to escape these from attribute serializations.
+      /** @var string[] */
+      $specialCharacters = [StringHelpers::ASCII_RECORD_SEPARATOR, StringHelpers::ASCII_GROUP_SEPARATOR];
+
+      // Loop through all the users and form the hash.
+      $isFirstUser = TRUE;
+      foreach ($pages as $page) {
+        /** @var int[] $page */
+        // Load all entities for this page.
+        /** @var \Drupal\user\UserInterface[] */
+        $users = $userStorage->loadMultiple($page);
+        // Loop through the UIDs.
+        foreach ($page as $uid) {
+          if (!$isFirstUser) {
+            hash_update($hashContext, StringHelpers::ASCII_GROUP_SEPARATOR);
+          }
+
+          if (!array_key_exists($uid, $users)) {
+            goto next_user;            
+          }
+          $user = $users[$uid];
+          if (!$userFilter($user)) {
+            goto next_user;
+          }
+
+          $isFirstAttribute = TRUE;
+          foreach ($sortedAttributeTypes as $attributeType) {
+            if (!$isFirstAttribute) {
+              hash_update($hashContext, StringHelpers::ASCII_RECORD_SEPARATOR);
+            }
+            $attributeSerialization = $binaryAttributeRepresentationRetrieval($user);
+            if (!is_string($attributeSerialization)) {
+              throw new \LogicException('The $binaryAttributeRepresentationRetrieval returned a non-string attribute serialization.');
+            }
+            $escapedAttributeSerialization = StringHelpers::escape($attributeSerialization, $specialCharacters);
+            hash_update($hashContext, $escapedAttributeSerialization);
+            $isFirstAttribute = FALSE;
+          }
+
+next_user:
+          $isFirstUser = FALSE;
+        }
+        
+        // Reset the static cache, just in case our attempt to disable it
+        // failed.
+        $userStorage->resetCache();
+      }
+
+    }
+    finally {
+      // Restore user static caching to previous setting.
+      $entityType->set('static_cache', $wasStaticCacheEnabled);
+    }
+
+    return hash_final($hashContext, TRUE);
+  }
+
+}
---
+++ tests/src/Helper/Drupal/UserHelpersTest.php
@@ -0,0 +1,162 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ranine\Tests\Helper\Drupal;
+
+use Drupal\user\UserInterface;
+use PHPUnit\Framework\TestCase;
+use Ranine\Exception\InvalidOperationException;
+use Ranine\Helper\Drupal\UserHelpers;
+use Ranine\Iteration\ExtendableIterable;
+use Ranine\Testing\Traits\MockEntityTypeManagerCreationTrait;
+
+/**
+ * Tests the UserHelpers class.
+ *
+ * @coversDefaultClass \Ranine\Helper\Drupal\UserHelpers
+ * @group ranine
+ */
+class UserHelpersTest extends TestCase {
+
+  use MockEntityTypeManagerCreationTrait;
+
+  /**
+   * Tests the getAttributeHash() method.
+   *
+   * @covers ::getAttributeHash
+   */
+  public function testGetAttributeHash() : void {
+    // Create a binary representation retrieval that handles the email, user
+    // status, and user timezone attributes.
+    $binaryRepresentationRetrieval = function (UserInterface $user, string $attributeType) {
+      if ($attributeType === 'email') {
+        return $user->getEmail() ?? '\0';
+      }
+      elseif ($attributeType === 'status') {
+        return $user->isActive() ? 'A' : 'B';
+      }
+      elseif ($attributeType === 'timezone') {
+        return $user->getTimeZone();
+      }
+      else {
+        throw new \InvalidArgumentException('Invalid attribute type requested.');
+      }
+    };
+
+    // Create a few mock users.
+    $users = new \SplFixedArray(3);
+    $users[0] = $this->getMockUser(2, 'eve@example.com', TRUE, "\eE\x1Dv\x1E\e\ee");
+    $users[1] = $this->getMockUser(3, 'cain@example.com', FALSE, 'pst');
+    $users[2] = $this->getMockUser(5, 'abel@example.com', FALSE, 'pst');
+
+    // Create a mock entity type manager for the three users.
+    $mockEntityTypeManager = $this->getMockEntityTypeManager(['user' => [
+      'storage_interface' => '\\Drupal\\user\\UserStorageInterface',
+      'entities' => ExtendableIterable::from($users)
+        ->map(fn($i, $u) => $u, fn($i, UserInterface $u) => (int) $u->id())
+        ->toArray(),
+    ]]);
+    // We also want to define the getEntityType() method on the mock storage to
+    // handle static caching stuff.
+    $mockEntityType = $this->createMock('\\Drupal\\Core\\Entity\\EntityTypeInterface');
+    $mockEntityType->method('isStaticallyCacheable')->willReturn(TRUE);
+    /** @var \PHPUnit\Framework\MockObject\MockObject|\Drupal\user\UserStorageInterface */
+    $mockStorage = $mockEntityTypeManager->getStorage('user');
+    $mockStorage->method('getEntityType')->willReturn($mockEntityType);
+    $mockStorage->method('resetCache')->willReturnCallback(function () : void {});
+
+    // Compute the expected binary representation with the email, status, and
+    // timezone. Skip UID = 3. Note:
+    // User separator = ASCII group sep = 0x1D
+    // Attribute separator = ASCII record sep = 0x1E
+    $binaryRepresentation =
+      'eve@example.com' . "\x1E" . 'A' . "\x1E" . "\e\eE\e\x1Dv\e\x1E\e\e\e\ee" . "\x1D" .
+      'abel@example.com' . "\x1E" . 'B' . "\x1E" . 'pst';
+    $expectedHash = hash('sha1', $binaryRepresentation, TRUE);
+
+    $hash = UserHelpers::getAttributeHash($mockEntityTypeManager,
+      [2, 3, 5],
+      ['email', 'status', 'timezone'],
+      $binaryRepresentationRetrieval,
+      fn(UserInterface $user) => $user->id() === 3 ? FALSE : TRUE);
+
+    $this->assertTrue($hash === $expectedHash);
+  }
+
+  /**
+   * Creates a mock \Drupal\user\UserInterface object.
+   *
+   * The returned object defins the following methods:
+   * - id() -- returns $uid
+   * - getEmail() -- returns $email
+   * - isActive() -- returns $isActive
+   * - isBlocked() -- returns !$isActive
+   * - isAuthenticated() -- returns TRUE
+   * - isAnonymous() -- returns FALSE
+   * - getTimeZone() -- returns $timezone
+   * - uuid() - returns $uuid
+   * - getDisplayName() - returns $displayName
+   * - getPreferredLangcode(FALSE) - returns $preferredLangcode
+   * - getPreferredAdminLangcode(FALSE) - returns $preferredAdminLangcode
+   *
+   * @param int $uid
+   *   User ID (positive number).
+   * @param string|null $email
+   *   Email, or NULL for no email.
+   * @param bool $isActive
+   *   User status.
+   * @param string $timezone
+   *   User timezone.
+   * @param string $uuid
+   *   UUID.
+   * @param string $displayName
+   *   Display name.
+   * @param string $preferredLangcode
+   *   Preferred langcode.
+   * @param string $preferredAdminLangcode
+   *   Preferred admin langcode.
+   *
+   * @return \PHPUnit\Framework\MockObject\MockObject|\Drupal\user\UserInterface
+   *   Mock user.
+   */
+  private function getMockUser(int $uid,
+    ?string $email = NULL,
+    bool $isActive = TRUE,
+    string $timezone = '',
+    string $uuid = '',
+    string $displayName = '',
+    string $preferredLangcode = '',
+    string $preferredAdminLangcode = '') {
+    /** @var \PHPUnit\Framework\MockObject\MockObject|\Drupal\user\UserInterface */
+    $mockUser = $this->createMockNoAutoMethodConfig('\\Drupal\\user\\UserInterface');
+    $mockUser->method('id')->willReturn($uid);
+    $mockUser->method('getEmail')->willReturn($email);
+    $mockUser->method('isActive')->willReturn($isActive);
+    $mockUser->method('isBlocked')->willReturn(!$isActive);
+    $mockUser->method('isAuthenticated')->willReturn(TRUE);
+    $mockUser->method('isAnonymous')->willReturn(FALSE);
+    $mockUser->method('getTimeZone')->willReturn($timezone);
+    $mockUser->method('uuid')->willReturn($uuid);
+    $mockUser->method('getDisplayName')->willReturn($displayName);
+    $mockUser->method('getPreferredAdminLangcode')->willReturnCallback(function (bool $fallback_to_default) use ($preferredAdminLangcode) : string {
+      if ($fallback_to_default) {
+        throw new InvalidOperationException('Method functionality not implemented.');
+      }
+      else {
+        return $preferredAdminLangcode;
+      }
+    });
+    $mockUser->method('getPreferredLangcode')->willReturnCallback(function (bool $fallback_to_default) use ($preferredLangcode) : string {
+      if ($fallback_to_default) {
+        throw new InvalidOperationException('Method functionality not implemented.');
+      }
+      else {
+        return $preferredLangcode;
+      }
+    });
+
+    return $mockUser;
+  }
+
+}
