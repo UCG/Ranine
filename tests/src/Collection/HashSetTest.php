bb6e3e9174a228a1c343a76eed7de4632679ea2c
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2022-01-19 11:10:20 -0500

    Added hash set test class.

---
+++ tests/src/Collection/HashSetTest.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ranine\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Ranine\Collection\HashSet;
+use Ranine\Iteration\ExtendableIterable;
+
+/**
+ * Tests the HashSet class.
+ *
+ * @coversDefaultClass \Ranine\HashSet
+ * @group ranine
+ */
+class HashSetTest extends TestCase {
+
+  /**
+   * Provides arguments for testAdd().
+   *
+   * @return array
+   *   Arguments.
+   */
+  public function provideTestAddArguments() : array {
+    return [
+      [[
+        'jkuu',
+        'jk',
+        'uiu',
+      ]],
+      [[
+        0,
+        4,
+        5,
+      ]],
+      [[
+        0.0,
+        -1.0,
+        -1.01,
+        1,
+      ]],
+      [[
+        ['a' => 'b', 'c' => 'd'],
+        ['a' => 'c', 'c' => 'e'],
+        ['a' => 'b', 'c' => 'd', 'e' => 'f'],
+        ['c' => 'd', 'a' => 'b'],
+        ['a' => 'b'],
+        ['c' => 'd'],
+        [0, 1, 2],
+        ['a', 'b', 'c', 'd'],
+        ['a', 'b'],
+        ['a', 'c'],
+      ]]
+    ];
+  }
+
+  /**
+   * Tests the add() and has() methods.
+   *
+   * Uses the default hashing / comparison.
+   *
+   * @covers ::add
+   * @covers ::has
+   * @dataProvider provideItemsToAdd
+   *
+   * @param array $items
+   *   Items to add.
+   */
+  public function testAdd($items) : void {
+    $set = new HashSet();
+    $oneIterationCompleted = FALSE;
+    foreach ($items as $item) {
+      if (!$oneIterationCompleted) {
+        $this->assertTrue($set->add($item));
+      }
+      $set->add($item);
+      $this->assertFalse($set->add($item));
+    }
+    
+    $this->assertTrue(ExtendableIterable::from($items)->all(fn($k, $item) => $set->has($item)));
+  }
+
+  /**
+   * Tests the getCount() method.
+   *
+   * Uses the default hashing / comparison.
+   *
+   * @covers ::getCount
+   */
+  public function testGetCount() : void {
+    $set = new HashSet();
+    $this->assertTrue($set->getCount() === 0);
+    $set->add(2);
+    $set->add(0);
+    $this->assertTrue($set->getCount() === 2);
+  }
+
+  /**
+   * Tests the remove() and has() method.
+   *
+   * Uses the default hashing / comparison.
+   *
+   * @covers ::remove
+   * @covers ::has
+   */
+  public function testRemove() : void {
+    $set = new HashSet();
+    $this->assertFalse($set->remove(0));
+    $set->add(3);
+    $set->add(5);
+    $this->assertTrue($set->remove(5));
+    $this->assertFalse($set->has(5));
+    $this->assertTrue($set->has(3));
+  }
+
+}
