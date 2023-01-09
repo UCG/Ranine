ffa34a882ac4b3f4ef36dbfca7e4ef166e2a960b
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2023-01-08 21:32:48 -0500

    "Merged" StackTrait into Stack since we're using generics now.

--- src/Collection/Stack.php
+++ src/Collection/Stack.php
@@ -4,18 +4,26 @@ declare(strict_types = 1);
 
 namespace Ranine\Collection;
 
+use Ranine\Exception\InvalidOperationException;
+
 /**
  * Represents a stack (FILO collection).
  */
 class Stack implements \IteratorAggregate {
 
-  use StackTrait;
+  /**
+   * Underlying array.
+   *
+   * Could also be implemented with a linked list, which might be useful for
+   * large stacks.
+   */
+  private array $arr = [];
 
   /**
    * Returns (but does not remove) the top element of the stack.
    */
   public function peek() : mixed {
-    return $this->peekInternal();
+    return end($this->arr);
   }
 
   /**
@@ -25,14 +33,41 @@ class Stack implements \IteratorAggregate {
    *   Thrown if the stack is empty.
    */
   public function pop() : mixed {
-    return $this->popInternal();
+    if ($this->isEmpty()) {
+      throw new InvalidOperationException('Cannot pop from an empty stack.');
+    }
+
+    return array_pop($this->arr);
   }
 
   /**
    * Pushes $element onto the stack.
    */
   public function push($element) : void {
-    $this->pushInternal($element);
+    array_push($this->arr, $element);
+  }
+
+  /**
+   * Gets an iterator to iterate through the elements of this stack.
+   *
+   * Iteration proceeds from the first element pushed to the last.
+   *
+   * @return \Traversable
+   *   Iterator.
+   */
+  public function getIterator() : \Traversable {
+    return (function () {
+      foreach ($this->arr as $element) {
+        yield $element;
+      }
+    })();
+  }
+
+  /**
+   * Tells whether the stack is empty.
+   */
+  public function isEmpty() : bool {
+    return empty($this->arr);
   }
 
 }
