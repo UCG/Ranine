e9257e153cbf60b9f56285876dc32e044dfe2b2d
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2025-03-17 14:54:13 -0400

    StringHelpersTest stub.

---
+++ tests/src/Helper/StringHelpersTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ranine\Tests\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Ranine\Helper\StringHelpers;
+
+/**
+ * Tests the StringHelpers class.
+ *
+ * @coversDefaultClass \Ranine\Helper\StringHelpers
+ * @group ranine
+ */
+class StringHelpersTest extends TestCase {
+}
