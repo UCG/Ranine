17a8d560886c027c5b17875208e3672be266c068
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2025-05-01 16:33:51 -0400

    AT-5 - New testAll() method.

--- tests/src/Iteration/ExtendableIterableTest.php
+++ tests/src/Iteration/ExtendableIterableTest.php
@@ -16,13 +16,26 @@ use Ranine\Iteration\ExtendableIterable;
 class ExtendableIterableTest extends TestCase {
 
   /**
-   * Test the all() method with a collection with a single valid item.
+   * Test the all() method.
    *
    * @covers ::all
+   * @dataProvider provideDataForTestAll
    */
-  public function testAllSingleValidInput() : void {
-    $this->assertTrue(ExtendableIterable::fromKeyAndValue('a', 'b')
-      ->all(fn($k, $v) => $k === 'a' && $v === 'b'));
+  public function testAll(array $inputData, callable $predicate, bool $expectedResult) : void {
+    $iter = ExtendableIterable::from($inputData);
+    $this->assertSame($expectedResult, $iter->all($predicate));
+  }
+
+  public function provideDataForTestAll() : array {
+    return [
+      'empty' => [[], fn() => FALSE, TRUE],
+      'single-false-predicate' => [[1], fn() => FALSE, FALSE],
+      'single-true-predicate' => [[1], fn() => TRUE, TRUE],
+      'double-key-only-true-predicate' => [[2 => 4, 4 => 6], fn(int $k) => $k % 2 === 0, TRUE],
+      'double-key-only-false-predicate' =>[[2 => 4, 3 => 6], fn(int $k) => $k % 2 === 1, FALSE],
+      'normal-false-predicate' => [[1 => 2, 3 => 4], fn(int $k, int $v) => $k + $v < 7, FALSE],
+      'normal-true-predicate' => [[1 => 2, 3 => 4], fn(int $k, int $v) => $k + $v <= 7, TRUE],
+    ];
   }
 
 }
