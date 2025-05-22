3dee0f2968e36ce838251968509aeba918c36be0
Jonathan Api <jonathan_api@ucg.org>
2025-05-22 14:18:23 -0400

    Started writing ExtendableIterable test

---
+++ tests/src/Iteration/ExtendableIterableTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ranine\Tests\Iteration;
+
+use PHPUnit\Framework\TestCase;
+use Ranine\Iteration\ExtendableIterable;
+
+/**
+ * Tests the ExtendableIterable class.
+ * 
+ * @coversDefaultClass \Ranine\Iteration\ExtendableIterable
+ * @group ranine
+ */
+class ExtendableIterableTest extends TestCase {
+
+
+}
