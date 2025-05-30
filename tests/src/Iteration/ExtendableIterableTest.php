88b20998bd9a93568f24167860c8746d4e747273
Jonathan Api <jonathan_api@ucg.org>
2025-05-30 14:21:27 -0400

    Added Append test

--- tests/src/Iteration/ExtendableIterableTest.php
+++ tests/src/Iteration/ExtendableIterableTest.php
@@ -26,6 +26,42 @@ class ExtendableIterableTest extends TestCase {
     $this->assertSame($expectedResult, $iter->all($predicate));
   }
 
+  /**
+   * Test the any() method.
+   * 
+   * @covers ::any
+   * @dataProvider provideDataForTestAny
+   */
+  public function testAny(array $inputData, callable $predicate, bool $expectedResult) : void {
+    $iter = ExtendableIterable::from($inputData);  
+    $this->assertSame($expectedResult, $iter->any($predicate));
+  }
+
+  /**
+   * Test the append() method.
+   * 
+   * @covers ::append
+   */
+  public function testAppend() : void {
+    $iter = ExtendableIterable::from([1, 3]);
+    $iterToAppend = [3, 4, 5];
+    $appendedIter = $iter->append($iterToAppend);
+    $i = 0;
+    $expectedValues = [1, 3, 3, 4, 5];
+    $expectedKeys = [0, 1, 0, 1, 2];
+    foreach ($appendedIter as $key => $value) {
+      $this->assertSame($expectedValues[$i], $value);
+      $this->assertSame($expectedKeys[$i], $key);
+      $i++;
+    }
+    $this->assertSame(5, $i);
+  
+  }
+
+public function testAppendKeyAndValue() : void {
+  $iter
+}
+
   public function provideDataForTestAll() : array {
     return [
       'empty' => [[], fn() => FALSE, TRUE],
@@ -35,6 +71,18 @@ class ExtendableIterableTest extends TestCase {
       'double-key-only-false-predicate' =>[[2 => 4, 3 => 6], fn(int $k) => $k % 2 === 1, FALSE],
       'normal-false-predicate' => [[1 => 2, 3 => 4], fn(int $k, int $v) => $k + $v < 7, FALSE],
       'normal-true-predicate' => [[1 => 2, 3 => 4], fn(int $k, int $v) => $k + $v <= 7, TRUE],
+    ];
+  }
+
+  public function provideDataForTestAny() : array {
+    return [
+      'empty' => [[], fn() => TRUE, FALSE],
+      'single-false-predicate' => [[1], fn() => FALSE, FALSE],
+      'single-true-predicate' => [[1], fn() => TRUE, TRUE],
+      'double-key-only-true-predicate' => [[2 => 4, 3 => 6], fn(int $k) => $k % 2 === 0, TRUE],
+      'double-key-only-false-predicate' =>[[5 => 4, 3 => 6], fn(int $k) => $k % 2 === 0, FALSE],
+      'normal-false-predicate' => [[1 => 2, 3 => 4], fn(int $k, int $v) => $k + $v < 1, FALSE],
+      'normal-true-predicate' => [[1 => 2, 3 => 4], fn(int $k, int $v) => $k + $v === 7, TRUE],
     ];
   }
 
