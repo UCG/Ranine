be037f38e2949f6c2b1808e940e3f2ff41e5ab52
Jonathan Api <jonathan_api@ucg.org>
2025-07-17 11:53:53 -0400

    AT-13 created stubs for all tests

--- tests/src/Helper/ParseHelpersTest.php
+++ tests/src/Helper/ParseHelpersTest.php
@@ -8,4 +8,62 @@ use Ranine\Exception\ParseException;
 use Ranine\Iteration\ExtendableIterable;
 use PHPUnit\Framework\TestCase;
 
+class ParseHelpersTest extends TestCase {
 
+  /**
+   * @covers ::parseInt
+   */
+  public function testParseInt() : void {
+
+  }
+
+  /**
+   * @covers ::parseIntFromString
+   */
+  public function testParseIntFromString() : void {
+
+  }
+
+  /**
+   * @covers ::parseIntRange
+   */
+  public function testParseIntRange() : void {
+
+  }
+
+  /**
+   * @covers ::parseIntRangeEndpoints
+   */
+  public function testParseIntRangeEndpoints() : void {
+
+  }
+
+  /**
+   * @covers ::tryParseInt
+   */
+  public function testTryParseInt() : void {
+
+  }
+
+  /**
+   * @covers ::tryParseIntFromString
+   */
+  public function testTryParseIntFromString() : void {
+
+  }
+
+  /**
+   * @covers ::tryParseIntRange
+   */
+  public function testTryParseIntRange() : void {
+
+  }
+
+  /**
+   * @covers ::tryParseIntRangeEndpoints
+   */
+  public function testTryParseIntRangeEndpoints() : void {
+
+  }
+
+}
