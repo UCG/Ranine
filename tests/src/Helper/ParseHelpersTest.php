18bd1870a7f03a97aa681c7ac00531a3b0672cd9
Jonathan Api <jonathan_api@ucg.org>
2025-07-21 13:37:07 -0400

    AT-13 wrote tests for parseInt,  parseIntFromString, parseIntRange

--- tests/src/Helper/ParseHelpersTest.php
+++ tests/src/Helper/ParseHelpersTest.php
@@ -7,6 +7,7 @@ namespace Ranine\Tests\Helper;
 use Ranine\Exception\ParseException;
 use Ranine\Iteration\ExtendableIterable;
 use PHPUnit\Framework\TestCase;
+use Ranine\Helper\ParseHelpers;
 
 class ParseHelpersTest extends TestCase {
 
@@ -14,21 +15,30 @@ class ParseHelpersTest extends TestCase {
    * @covers ::parseInt
    */
   public function testParseInt() : void {
-
+    // Input:
+    // Number: 5
+    // Result: int 5
+    $this->assertEquals(5, ParseHelpers::parseInt(5));
   }
 
   /**
    * @covers ::parseIntFromString
    */
   public function testParseIntFromString() : void {
-
+    // Input:
+    // String number: '5'
+    // Result: int 5
+    $this->assertEquals(5, ParseHelpers::parseInt('5'));
   }
 
   /**
    * @covers ::parseIntRange
    */
   public function testParseIntRange() : void {
-
+    $divider = '-';
+    $range = "[0]$divider[5]";
+    $expectedResult = [0, 1, 2, 3, 4, 5];
+    $this->assertEquals($expectedResult, ParseHelpers::parseIntRange($range, $divider));
   }
 
   /**
