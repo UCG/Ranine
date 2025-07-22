6c7d62275e009bcddb957fb09f6dd68344ad0c32
Jonathan Api <jonathan_api@ucg.org>
2025-07-22 14:30:57 -0400

    AT-13 made tests and dataproviders for parseIntFromString and parseIntRange

--- tests/src/Helper/ParseHelpersTest.php
+++ tests/src/Helper/ParseHelpersTest.php
@@ -7,7 +7,7 @@ namespace Ranine\Tests\Helper;
 use PHPUnit\Framework\TestCase;
 use Ranine\Exception\ParseException;
 use Ranine\Helper\ParseHelpers;
-use Ranine\Tests\Trait\IterableAssertionTrait;
+use Ranine\Tests\Traits\IterableAssertionTrait;
 
 class ParseHelpersTest extends TestCase {
 
@@ -32,22 +32,28 @@ class ParseHelpersTest extends TestCase {
 
   /**
    * @covers ::parseIntFromString
+   * @dataProvider provideDataForTestParseIntFromString
    */
-  public function testParseIntFromString() : void {
-    // Input:
-    // String number: '5'
-    // Result: int 5
-    $this->assertEquals(5, ParseHelpers::parseInt('5'));
+  public function testParseIntFromString(string $inputString, int $expectedResult) : void {
+    $this->assertEquals($expectedResult, ParseHelpers::parseIntFromString($inputString));
+  }
+
+  /**
+   * @covers ::parseIntFromString
+   * @dataProvider provideDataForTestParseIntFromStringInvalid
+   */
+  public function testParseIntFromStringInvalid(string $valueToTryToParse) : void {
+    $this->expectException(ParseException::class);
+    ParseHelpers::parseIntFromString($valueToTryToParse);
   }
 
   /**
    * @covers ::parseIntRange
+   * @dataProvider provideDataForTestParseIntRange
    */
-  public function testParseIntRange() : void {
-    $divider = '-';
-    $range = '0-5';
-    $expectedResult = [0, 1, 2, 3, 4, 5];
-    $this->assertEquals($expectedResult, ParseHelpers::parseIntRange($range, $divider));
+  public function testParseIntRange(string $start, string $divider, string $end, array $expectedValues, int $expectedCount) : void {
+    $range = $start . $divider . $end;
+    $this->assertIterableValues(ParseHelpers::parseIntRange($range, $divider), $expectedValues, $expectedCount);
   }
 
   /**
@@ -106,4 +112,41 @@ class ParseHelpersTest extends TestCase {
     ];
   }
 
+  public function provideDataForTestParseIntFromString() : array {
+    return [
+      'negative-int' => ['-9', -9],
+      'positive-int' => ['77', 77],
+    ];
+  }
+
+  public function provideDataForTestParseIntFromStringInvalid() : array {
+    return [
+      'bool' => ['True'],
+      'letters' => ['a1a'],
+      'symbols' => ['@'],
+      'float' => ['0.01']
+    ];
+  }
+
+  public function provideDataForTestParseIntRange() : array {
+    return [
+      'start-end-negative' => ['-7', '/', '-2', [-7, -6, -5, -4, -3, -2], 6],
+      'same-start-and-end' => ['0', '|', '0', [0], 1],
+      'ordinary-start-divider-and-end' => ['1', 'A', '9', [1, 2, 3, 4, 5, 6, 7, 8, 9], 9],
+    ];
+  }
+  
+  public function provideDataForTestParseIntRangeInvalid() : array {
+    return [
+      'start-greater-than-end' => ['5', '-', '3'],
+      'start-divider-same' => ['7', '7', '12'],
+      'invalid-divider' => [],
+      'empty-divider' => [],
+      'empty-start' => [],
+      'empty-end' => [],
+      'start-is-float' => [],
+      'end-is-bool' => [],
+    ];
+  }
+   
 }
