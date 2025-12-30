b20e080912b1634183add4d61febf90e6f7b26f5
Jonathan Api <jonathan_api@ucg.org>
2025-12-30 11:02:20 -0500

    AT-39 - Replaced Annotations with Attributes in StringPartTest

--- tests/src/StringPartTest.php
+++ tests/src/StringPartTest.php
@@ -4,22 +4,21 @@ declare(strict_types = 1);
 
 namespace Ranine\Tests;
 
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversFunction;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Ranine\StringPart;
 
-/**
- * Tests the StringPart class.
- *
- * @coversDefaultClass \Ranine\StringPart
- * @group ranine
- */
+#[TestDox('Tests the StringPart class')]
+#[CoversClass(StringPart::class)]
+#[Group('ranine')]
 class StringPartTest extends TestCase {
 
-  /**
-   * Tests the append() method.
-   *
-   * @covers ::append
-   */
+  #[TestDox('Tests the append() method.')]
+  #[CoversFunction('append')]
   public function testAppend() : void {
     $firstSentence = 'In a sort of ghastly simplicity we remove the organ and demand the function. ';
     $secondSentence = 'We make men without chests and expect of them virtue and enterprise.';
@@ -39,11 +38,8 @@ class StringPartTest extends TestCase {
     $this->assertTrue(((string) $part) === $comparison);
   }
 
-  /**
-   * Tests the clean() method.
-   *
-   * @covers ::clean
-   */
+  #[TestDox('Tests the clean() method.')]
+  #[CoversFunction('clean')]
   public function testClean() : void {
     $sentence = 'But the Court has cheated both sides, robbing the winners of an honest victory, and the losers of the peace that comes from a fair defeat.';
     // ~ A. Scalia, J., dissenting in United States v. Windsor
@@ -53,38 +49,28 @@ class StringPartTest extends TestCase {
     $this->assertTrue($part->getBackingString() === $sentence);
   }
   
-  /**
-   * @covers ::clear
-   */
+  #[CoversFunction('clear')]
   public function testClear() : void {
     $part = StringPart::create('Delete this text!', 0, 16)->clear();
     $expected = '';
     $this->assertTrue($part->equals($expected));
   }
 
-  /**
-   * @covers ::create
-   *
-   * @dataProvider provideDataForTestCreate
-   */
+  #[CoversFunction('create')]
+  #[DataProvider('provideDataForTestCreate')]
   public function testCreate(string $backingString, int $startPosition, int $endPosition, string $expectedString) : void {
     $this->assertSame($expectedString, (string)StringPart::create($backingString, $startPosition, $endPosition));
   }
   
-  /**
-   * @covers ::create
-   * @dataProvider provideDataForTestCreateInvalid
-   */
+  #[CoversFunction('create')]
+  #[DataProvider('provideDataForTestCreateInvalid')]
   public function testCreateInvalid(string $backingString, int $startPosition, int $endPosition) : void {
     $this->expectException('\InvalidArgumentException');
     StringPart::create($backingString, $startPosition, $endPosition);
   }
   
-  /**
-   * Tests the equals() method.
-   *
-   * @covers ::equals
-   */
+  #[TestDox('Tests the equals() method.')]
+  #[CoversFunction('equals')]
   public function testEquals() : void {
     $firstPart = 'It occurred to him that those scarcely perceptible impulses of his to protest what people of high rank considered good,';
     $secondPart = 'vague impulses which he had always suppressed, might have been precisely what mattered,';
@@ -95,11 +81,8 @@ class StringPartTest extends TestCase {
     $this->assertTrue($part->equals($secondPart));
   }
 
-  /**
-   * Tests the equalsStringPart() method.
-   *
-   * @covers ::equalsStringPart
-   */
+  #[TestDox('Tests the equalsStringPart() method.')]
+  #[CoversFunction('equalsStringPart')]
   public function testEqualsStringPart() : void {
     $firstSentence = 'There are a dozen views about everything until you know the answer.';
     $secondSentence = 'Then there\'s never more than one.';
@@ -113,46 +96,36 @@ class StringPartTest extends TestCase {
     $this->assertTrue($part3->equalsStringPart($part1));
   }
   
-  /**
-   * @covers ::getBackingString
-   */
+  #[CoversFunction('getBackingString')]
   public function testGetBackingString() : void {
     $part = StringPart::create('Somethin bit me!', 0, 7);
     $this->assertSame('Somethin bit me!', $part->getBackingString());
     // ~ Forest Gump
   }
   
-  /**
-   * @covers ::getEndPosition
-   */
+  #[CoversFunction('getEndPosition')]
   public function testGetEndPosition() : void {
     $part = StringPart::create('Somethin bit me!', 5, 10);
     $this->assertSame(10, $part->getEndPosition());
     // ~ Forest Gump
   }
 
-  /**
-   * @covers ::getLength
-   * @dataProvider provideDataForTestGetLength
-   */
+  #[CoversFunction('getLength')]
+  #[DataProvider('provideDataForTestGetLength')]
   public function testGetLength(int $startPosition, int $endPosition, int $expectedLength) : void {
     $part = StringPart::create('I have the ears of a fox and the eyes a hawk.', $startPosition, $endPosition);
     $this->assertSame($expectedLength, $part->getLength());
     // ~ J. R. Tolkien, Gimli, son of Glo'in, The Lord of the Rings: The Fellowship of the Ring
   }
 
-  /**
-   * @covers ::getStartPosition
-   * @dataProvider provideDataForTestGetStartPosition
-   */
+  #[CoversFunction('getStartPosition')]
+  #[DataProvider('provideDataForTestGetStartPosition')]
   public function testGetStartPosition(int $expectedStartPosition, int $startPosition, int $endPosition) : void {
     $part = StringPart::create('From your starting position, play pawn to e4.', $startPosition, $endPosition);
     $this->assertSame($expectedStartPosition, $part->getStartPosition());
   }
 
-  /**
-   * @covers ::isEmpty
-   */
+  #[CoversFunction('isEmpty')]
   public function testIsEmpty() : void {
     $part = StringPart::create(' ', -1,-1);
     $this->assertTrue($part->isEmpty());
@@ -160,11 +133,8 @@ class StringPartTest extends TestCase {
     $this->assertFalse($part->isEmpty());
   }
 
-  /**
-   * Tests the recut() method.
-   *
-   * @covers ::recut
-   */
+  #[TestDox('Tests the recut() method.')]
+  #[CoversFunction('recut')]
   public function testRecut() : void {
     $firstSentence = 'We laugh at honour and are shocked to find traitors in our midst. ';
     $secondSentence = 'We castrate and bid the geldings be fruitful.';
@@ -176,11 +146,8 @@ class StringPartTest extends TestCase {
     $this->assertTrue(((string) $part) === $firstSentence);
   }
 
-  /**
-   * Tests the withNewEndpoints() method.
-   *
-   * @covers ::withNewEndpoints
-   */
+  #[TestDox('Tests the withNewEndpoints() method.')]
+  #[CoversFunction('withNewEndpoints')]
   public function testWithNewEndpoints() : void {
     $firstSentence = 'The whole point of seeing through something is to see something through it. ';
     $secondSentence = 'To "see through" all things is the same as not to see.';
