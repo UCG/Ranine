4b8fe38f0359d980deee70702a9e48e3baf402cf
Jonathan Api <jonathan_api@ucg.org>
2025-12-31 16:16:49 -0500

    AT-39 - Replaced PHPUnit Annotations with Attributes in EqualityHelpersTest

--- tests/src/Helper/EqualityHelpersTest.php
+++ tests/src/Helper/EqualityHelpersTest.php
@@ -4,22 +4,20 @@ declare(strict_types = 1);
 
 namespace Ranine\Tests\Helper;
 
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversFunction;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Ranine\Helper\EqualityHelpers;
 
-/**
- * Tests the EqualityHelpers class.
- *
- * @coversDefaultClass \Ranine\Helper\EqualityHelpers
- * @group ranine
- */
+#[TestDox('Tests the EqualityHelpers class.')]
+#[CoversClass(EqualityHelpers::class)]
+#[Group('ranine')]
 class EqualityHelpersTest extends TestCase {
 
-  /**
-   * Tests the areArraysEqualStrictOrderInvariant() method.
-   *
-   * @covers ::areArraysEqualStrictOrderInvariant
-   */
+  #[TestDox('Tests the areArraysEqualStrictOrderInvariant() method.')]
+  #[CoversFunction('areArraysEqualStrictOrderInvariant')]
   public function testArrayEqualityStrictOrderInvariant() : void {
     $obj = new \stdClass();
     $baseArray = ['4' => $obj, 5 => [$obj, 2, 4, 5.0], 2 => 7, 3 => ['2', ['a' => 0, 'b' => 'happy']]];
